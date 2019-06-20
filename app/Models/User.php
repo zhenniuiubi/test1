71c96e568b5a6806e150716495abd7fb776cbf85
247399462@qq.com <247399462@qq.com>
2019-06-20 17:47:35 +0800

    先删除所有文件夹 一步一步执行 最后 用db:seed创建 伪数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -28,7 +25,17 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
-    public function gravatar($size='100')
+    //boot 方法会在用户模型类完成初始化之后进行加载，因此我们对事件的监听需要放在该方法中。
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->activation_token = str_random(30);
+        });
+    }
+
+    public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,12 +14,15 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
-    // static $password;
-
+    $date_time = $faker->date . ' ' . $faker->time;
     return [
-        'name' => $faker->unique()->username,
+        'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => 123456,
+        'email_verified_at' => now(),
+        'activated' => true,
+        'password' => bcrypt(123456),
         'remember_token' => str_random(10),
+        'created_at' => $date_time,
+        'updated_at' => $date_time,
     ];
 });
