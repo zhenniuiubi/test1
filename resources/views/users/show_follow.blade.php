274d2ee09e427dd8fd834dd986240ca402f1e93b
247399462@qq.com <247399462@qq.com>
2019-06-24 11:51:12 +0800

    关注按钮

--- resources/views/users/show_follow.blade.php
+++ resources/views/users/show_follow.blade.php
@@ -1,37 +1,21 @@
 @extends('layouts.default')
-@section('title',$user->name)
+@section('title',$title)
 
 @section('content')
-<div class="row">
-    <div class="offset-md-2 col-md-8">
-        <section class="user_info">
-            @include('shared._user_info',['user'=>$user])
-        </section>
+<div class="offset-md-2 col-md-8">
+    <h2 class="mb-4 text-center">{{$title}}</h2>
 
-        @if(Auth::check())
-        @include('users._follow_form')
-        @endif
-
-        <section class="stats mt-2">
-            @include('shared._stats',['user'=>$user])
-        </section>
-
-        <hr>
-
-        <section class="status">
-            @if($statuses->count() > 0)
-            <ul class="list-unstyled">
-                @foreach($statuses as $status)
-                @include('statuses._status')
-                @endforeach
-            </ul>
-            <div class="mt-5">
-                {!! $statuses->render() !!}
+    <div class="list-group list-group-flush">
+        @foreach($users as $user)
+            <div class="list-group-item">
+                <img src="{{$user->gravatar()}}" alt="{{$user->name}}" class="mr-3" width="32">
+                <a href="{{route('users.show',$user)}}">{{$user->name}}</a>
             </div>
-            @else
-            <p>没有数据！</p>
-            @endif
-        </section>
+        @endforeach
+    </div>
+
+    <div class="mt-3">
+        {!! $users->render() !!}
     </div>
 </div>
 @endsection
