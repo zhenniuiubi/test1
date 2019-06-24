138e597f9fe1f378bf509f57144683b240ac58e9
247399462@qq.com <247399462@qq.com>
2019-06-24 09:02:24 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,12 +57,28 @@ class User extends Authenticatable
     //粉丝列表
     public function followers()
     {
-        return $this->belongsToMany(User::class,'followers','user_id','follower_id');
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
     }
 
     //关注者列表
     public function followings()
     {
-        return $this->belongsToMany(User::class,'followers','follower_id','user_id');
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
     }
 }
